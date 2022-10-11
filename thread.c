e1f4b2255b72b23d8b19005c4455fade8c74e678
Arthur Gao <arthurerconggao@gmail.com>
2022-10-11 16:28:39 -0400

    FIX: yeild bugs

--- thread.c
+++ thread.c
@@ -142,7 +142,7 @@ thread_yield (Tid want_tid)
 
         return change_threads(&all_threads[next_thread]);
 
-    } else if (want_tid == THREAD_SELF || want_tid == thread_id()) {
+    } else if (want_tid == THREAD_SELF || want_tid == running->thread_id) {
         // Does nothing as current thread continues
         return thread_id();
     } else {
